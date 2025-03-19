11cec6a935c6c2dbfc19f9618e59b35151386a4c
Julian <julianshen2002@yahoo.de>
2025-03-19 10:01:25 +0100

    Adapt routes so the landing page is always at '/home'

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
+
 
 app: Flask = Flask(__name__)
 
-@app.route('/navbar')
-def navbar():
-    return render_template('navbar.html')
+@app.route("/")
+def index():
+    return redirect(url_for('home'))
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
 
 @app.route('/upload')
 def upload():
     return render_template('upload.html')
 
-@app.route("/")
-def index():
-    return render_template('home.html')
+@app.route('/navbar')
+def navbar():
+    return render_template('navbar.html')
+
 
 if __name__ == "__main__": 
     app.run(debug=True)
