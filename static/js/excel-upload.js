0acc9ce5f0499472b6452c1c23f75b7e53e54f98
Julian <julianshen2002@yahoo.de>
2025-08-10 20:34:48 +0200

    Add textboxes to enter email and identifier.

--- static/js/excel-upload.js
+++ static/js/excel-upload.js
@@ -3,6 +3,11 @@ const fileInput = document.getElementById("excel-file");
 const dropText = document.getElementById("drop-text");
 const fileCaption = document.getElementById("file-caption");
 
+const form = document.querySelector(".needs-validation");
+const fileFeedback = document.getElementById("file-invalid-feedback");
+
+fileFeedback.style.display = "none";
+
 dropZone.addEventListener("click", () => fileInput.click());
 
 dropZone.addEventListener("dragover", (e) => {
@@ -22,7 +27,8 @@ dropZone.addEventListener("drop", (e) => {
         fileInput.files = e.dataTransfer.files;
         dropText.textContent = e.dataTransfer.files[0].name;
         fileCaption.textContent = "";
-        dropZone.classList.add("uploaded"); 
+        dropZone.classList.add("uploaded");
+        fileFeedback.style.display = "none"; 
     }
 });
 
@@ -30,6 +36,44 @@ fileInput.addEventListener("change", () => {
     if (fileInput.files.length > 0) {
         dropText.textContent = fileInput.files[0].name;
         fileCaption.textContent = "";
-        dropZone.classList.add("uploaded"); 
+        dropZone.classList.add("uploaded");
+        fileFeedback.style.display = "none"; 
     }
 });
+
+if (form) {
+    form.addEventListener("submit", (event) => {
+        let fileSelected = fileInput.files && fileInput.files.length > 0;
+
+        const emailInput = document.getElementById("user-email");
+        const identifierInput = document.getElementById("user-identifier");
+
+        if (emailInput.checkValidity()) {
+            emailInput.classList.add("is-valid");
+            emailInput.classList.remove("is-invalid");
+        } else {
+            emailInput.classList.add("is-invalid");
+            emailInput.classList.remove("is-valid");
+        }
+
+        if (identifierInput.checkValidity()) {
+            identifierInput.classList.add("is-valid");
+            identifierInput.classList.remove("is-invalid");
+        } else {
+            identifierInput.classList.add("is-invalid");
+            identifierInput.classList.remove("is-valid");
+        }
+
+        if (!form.checkValidity() || !fileSelected) {
+            event.preventDefault();
+            event.stopPropagation();
+
+            if (!fileSelected) {
+                fileFeedback.style.display = "block";
+            }
+        }
+
+        form.classList.add("was-validated");
+    });
+}
+
